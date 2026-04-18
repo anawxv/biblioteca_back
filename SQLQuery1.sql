f65cabcb18dc8ff50fb3c3a94349e36a2427cde3
HidekiSeven <gustavokanomato@gmail.com>
2026-04-17 21:50:52 -0300

    Create tables for emprestimo, multa, and reserva
    Tabelas Independentes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,22 +1,37 @@
-CREATE TABLE Usuario (
-    id INT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    tipo VARCHAR(20) NOT NULL
-);
+CREATE TABLE emprestimo (
+    id_emprestimo SERIAL PRIMARY KEY,
 
-CREATE TABLE Livro (
-    id INT PRIMARY KEY,
-    titulo VARCHAR(150) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
-    emprestado BIT NOT NULL
-);
-
-CREATE TABLE Emprestimo (
-    id INT PRIMARY KEY IDENTITY,
-    id_usuario INT NOT NULL,
+    id_cliente INT NOT NULL,
     id_livro INT NOT NULL,
+    id_funcionario INT NOT NULL,
+
     data_emprestimo DATE NOT NULL,
+    data_prevista DATE NOT NULL,
     data_devolucao DATE,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id),
-    FOREIGN KEY (id_livro) REFERENCES Livro(id)
+
+    status VARCHAR(20),
+
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_livro) REFERENCES livro(id_livro),
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario)
+);
+
+CREATE TABLE multa (
+    id_multa SERIAL PRIMARY KEY,
+    id_emprestimo INT NOT NULL,
+    valor DECIMAL(10,2),
+    status VARCHAR(20),
+
+    FOREIGN KEY (id_emprestimo) REFERENCES emprestimo(id_emprestimo)
+);
+
+CREATE TABLE reserva (
+    id_reserva SERIAL PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_livro INT NOT NULL,
+    data_reserva DATE,
+    status VARCHAR(20),
+
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+    FOREIGN KEY (id_livro) REFERENCES livro(id_livro)
 );
